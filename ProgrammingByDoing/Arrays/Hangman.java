1f8e91685d27bc7b6020a6772685caf21413c159
Charles Cabral Matovu <charlescmatovu@gmail.com>
2020-09-09 21:59:25 -0400

    more exercises

--- ProgrammingByDoing/Arrays/Hangman.java
+++ ProgrammingByDoing/Arrays/Hangman.java
@@ -1,12 +1,13 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public interface Hangman {
+public class Hangman {
     public static void main(String[] args) {
         String[] words ={"god"};
         Random random = new Random();
         int getWord = random.nextInt(words.length);
         String wordToGuess = words[getWord];
+        String[] compare1 = new String[wordToGuess.length()];
         String guess ="";
         String misses ="";
         String[] word = new String[wordToGuess.length()];
@@ -26,20 +27,22 @@ public interface Hangman {
             if(wordToGuess.contains(guess)){
                 int index = wordToGuess.indexOf(String.valueOf(guess));
                 word[index]=guess;
+                compare1[index]=
+                wordToGuess = wordToGuess.replaceFirst(guess,"=");
+
             }else{
                 misses +=guess;
             }
-            String compare ="wer";
-            compare = word.toString();
-            System.out.println("sdfsasdfasdfasdfasd      "+compare);
-
-            if((compare.equals(wordToGuess))){
+            String compare2 =word[0];
+            if((compare2.equals(compare1))){
                 System.out.print("YOU GOT IT!\n\nPlay \"again\" or \"quit\"? ");
                 String response = scan.next();
                 if(response.equals("quit")){
                     break;
                 }
             }
+
+            System.out.println("word to guesss and compare "+wordToGuess+"  cop    "+compare1+"    cop2   "+compare2);
         }
 
     }
