a69e2372dd1d94581360a73ab32cde0312315aed
Charles Cabral Matovu <charlescmatovu@gmail.com>
2020-09-17 22:09:38 -0400

    Rename Folder

---
+++ BuildingJavaProgams/src/maxLength.java
@@ -0,0 +1,10 @@
+/*Write a method maxLength that takes an ArrayList of Strings as a parameter and that returns the length
+        of the longest string in the list. If your method is passed an empty list, it should return 0.*/
+
+
+public class maxLength {
+    public static void main(String[] args) {
+
+        System.out.println("sdfa");
+    }
+}
---
+++ BuildingJavaProgams1/maxLength.java
@@ -0,0 +1,5 @@
+public class maxLength {
+    public static void main(String[] args) {
+        System.out.println("sfdsadf");
+    }
+}
