81ed3fadc2e5b12d323312f60b0d2e02c06afdb4
Charles Cabral Matovu <charlescmatovu@gmail.com>
2020-09-17 22:16:44 -0400

    Finish Setup

---
+++ BuildingJavaProgams/src/MaxLength.java
@@ -0,0 +1,12 @@
+/*Write a method maxLength that takes an ArrayList of Strings as a parameter and that returns the length
+of the longest string in the list. If your method is passed an empty list, it should return 0.*/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MaxLength {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>(Arrays.asList("one","day","today","and","collections"));
+    }
+}
