771eb6feedf430a9ee6c7fd86d41c0466869ebf7
Charles Cabral Matovu <charlescmatovu@gmail.com>
2020-09-18 17:59:40 -0400

    added swappairs

---
+++ BuildingJavaProgams/src/SwapPairs.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SwapPairs {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>(Arrays.asList("four", "score", "and", "seven", "years", "ago"));
+        swapPairs(list);
+        System.out.println(list);
+    }
+
+    private static void swapPairs(ArrayList<String> list) {
+        for(int i=0;i<list.size();i+=2){
+            String element1= list.get(i);
+            String element2 = list.get(i+1);
+
+            list.set(i,element2);
+            list.set(i+1,element1);
+        }
+    }
+}
