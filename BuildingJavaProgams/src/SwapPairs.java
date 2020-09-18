3c22551665c5f5b09b1d70620ef5d452caced422
Charles Cabral Matovu <charlescmatovu@gmail.com>
2020-09-18 18:03:36 -0400

    fixed bug in swappairs

--- BuildingJavaProgams/src/SwapPairs.java
+++ BuildingJavaProgams/src/SwapPairs.java
@@ -3,13 +3,13 @@ import java.util.Arrays;
 
 public class SwapPairs {
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<String>(Arrays.asList("four", "score", "and", "seven", "years", "ago"));
+        ArrayList<String> list = new ArrayList<String>(Arrays.asList("four", "score", "and", "seven", "years"));
         swapPairs(list);
         System.out.println(list);
     }
 
     private static void swapPairs(ArrayList<String> list) {
-        for(int i=0;i<list.size();i+=2){
+        for(int i=0;i<list.size()-1;i+=2){
             String element1= list.get(i);
             String element2 = list.get(i+1);
 
