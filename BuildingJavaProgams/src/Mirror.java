1563644b0acfc8af43c5a1ac8fec77948a2a3050
Charles Cabral Matovu <charlescmatovu@gmail.com>
2020-09-19 17:29:23 -0400

    added exercise

--- BuildingJavaProgams/src/Mirror.java
+++ BuildingJavaProgams/src/Mirror.java
@@ -1,3 +1,11 @@
+/*
+Write a method mirror that accepts an ArrayList of Strings as a parameter and
+produces a mirrored copy of the list as output,
+with the original values followed by those same values in the opposite order.
+For example, if an ArrayList variable called list contains the values ["a", "b", "c"],
+after a call of mirror(list); it should contain ["a", "b", "c", "c", "b", "a"].
+ */
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
