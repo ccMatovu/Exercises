5e6d94fa3aabc492f9eff2b7211397bad52ef21d
Charles Cabral Matovu <charlescmatovu@gmail.com>
2020-09-18 22:11:41 -0400

    added exercise

---
+++ BuildingJavaProgams/src/RemoveDuplicates.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String >(Arrays.asList("be", "be", "is", "not", "or", "question", "that", "the", "to", "to"));
+        removeDuplicates(list);
+        System.out.println(list);
+    }
+
+    private static void removeDuplicates(ArrayList<String> list) {
+        for(int i=0;i<list.size()-1;i++){
+            for(int j=0;j<list.size();j++){
+                String element1 = list.get(i);
+                String element2 = list.get(j);
+                if(element1.equals(element2)){
+                    list.remove(j);
+                    
+                }
+            }
+        }
+    }
+}
