ac78edd79123e4599fe34f24fb28547048f88dab
Charles Cabral Matovu <charlescmatovu@gmail.com>
2020-09-19 12:37:54 -0400

    fixed issues

--- BuildingJavaProgams/src/Stutter.java
+++ BuildingJavaProgams/src/Stutter.java
@@ -1,3 +1,13 @@
+/*
+Write a method stutter that takes an ArrayList of Strings and an integer k as parameters and that
+replaces every string with k copies of that string.
+For example, if the list stores the values ["how", "are", "you?"] before the method is called
+and k is 4, it should store the values
+["how", "how", "how", "how", "are", "are", "are", "are", "you?", "you?", "you?", "you?"]
+after the method finishes executing. If k is 0 or negative, the list should be empty after the call.
+ */
+
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.SplittableRandom;
@@ -5,7 +15,7 @@ import java.util.SplittableRandom;
 public class Stutter {
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<String>(Arrays.asList("test1", "test2"));
-        int num = 0;
+        int num = 3;
         stutter(list,num);
         System.out.println(list);
     }
