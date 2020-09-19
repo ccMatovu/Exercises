d58d2da13001e333249c102137669c49c773b979
Charles Cabral Matovu <charlescmatovu@gmail.com>
2020-09-18 22:17:04 -0400

    fixed issues

--- BuildingJavaProgams/src/RemoveDuplicates.java
+++ BuildingJavaProgams/src/RemoveDuplicates.java
@@ -10,12 +10,12 @@ public class RemoveDuplicates {
 
     private static void removeDuplicates(ArrayList<String> list) {
         for(int i=0;i<list.size()-1;i++){
-            for(int j=0;j<list.size();j++){
-                String element1 = list.get(i);
+            String element1 = list.get(i);
+            for(int j=i+1;j<list.size();j++){
                 String element2 = list.get(j);
                 if(element1.equals(element2)){
                     list.remove(j);
-                    
+                    j=j-1;
                 }
             }
         }
