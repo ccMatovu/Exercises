9b721eb857be643b664f626c92b148298ccd2648
Charles Cabral Matovu <charlescmatovu@gmail.com>
2020-09-22 23:58:55 -0400

    Organize folders

--- BuildingJavaProgams/src/CountUnique.java
+++ BuildingJavaProgams/src/CountUnique.java
@@ -1,3 +1,12 @@
+/*
+Write a method countUnique that takes a List of integers as a parameter and returns the number of
+unique integer values in the list.
+Use a Set as auxiliary storage to help you solve this problem.
+For example, if a list contains the values [3, 7, 3, -1, 2, 3, 7, 2, 15, 15], your method should
+return 5.
+The empty list contains 0 unique values.
+ */
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
