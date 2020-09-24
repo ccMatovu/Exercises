59c5a1feac762cab4a2a844e5be0178026669773
Charles Cabral Matovu <charlescmatovu@gmail.com>
2020-09-23 22:20:53 -0400

    Added Exercise

--- BuildingJavaProgams/src/CountCommon.java
+++ BuildingJavaProgams/src/CountCommon.java
@@ -1,3 +1,12 @@
+/*
+Write a method countCommon that takes two Lists of integers as parameters and returns the number of
+unique integers that occur in both lists.
+Use one or more Sets as storage to help you solve this problem.
+For example, if one list contains the values [3, 7, 3, -1, 2, 3, 7, 2, 15, 15] and the other list
+contains the values [-5, 15, 2, -1, 7, 15, 36], your method should return 4
+(because the elements -1, 2, 7, and 15 occur in both lists).
+*/
+
 import java.util.*;
 
 public class CountCommon {
@@ -22,4 +31,12 @@ public class CountCommon {
         }
         return count;
     }
+  //  other solution
+//    private static int countCommon(List<Integer> list1, List<Integer> list11) {
+//        Set<Integer> check1 = new HashSet<>(list1);
+//        check1.retainAll(list11);
+//        return check1.size();
+//    }
+
+
 }
