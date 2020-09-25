db105a0a35defa9877e87f9c7cc537e015e5189b
Charles Cabral Matovu <charlescmatovu@gmail.com>
2020-09-24 22:36:49 -0400

    Added Exercise

--- BuildingJavaProgams/src/RemoveEvenLength2.java
+++ BuildingJavaProgams/src/RemoveEvenLength2.java
@@ -1,3 +1,15 @@
+/*
+Write a method removeEvenLength that takes a Set of strings as a parameter and that removes all of
+the strings of even length from the set.
+For example, if your method is passed a set containing the following elements:
+  ["foo", "buzz", "bar", "fork", "bort", "spoon", "!", "dude"]
+Your method should modify the set to store the following elements
+(the order of the elements does not matter):
+  ["foo", "bar", "spoon", "!"]
+ */
+
+
+
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
