0e18e2a4844723dc8c763cbd70fde436b9625d13
Charles Cabral Matovu <charlescmatovu@gmail.com>
2020-09-25 15:40:13 -0400

    Added Exercise

--- BuildingJavaProgams/src/MaxOccurrences.java
+++ BuildingJavaProgams/src/MaxOccurrences.java
@@ -1,3 +1,10 @@
+/*
+Write a method maxOccurrences that accepts a List of integers as a parameter and returns the number
+of times the most frequently occurring integer (the "mode") occurs in the list.
+Solve this problem using a Map as auxiliary storage.
+If the list is empty, return 0.
+*/
+
 import java.util.*;
 
 public class MaxOccurrences {
