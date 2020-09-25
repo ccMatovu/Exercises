4730eb9878ee1d55ad5840725b0f2348e7612a6a
Charles Cabral Matovu <charlescmatovu@gmail.com>
2020-09-24 22:30:58 -0400

    Added Exercise

--- BuildingJavaProgams/src/HasOdd.java
+++ BuildingJavaProgams/src/HasOdd.java
@@ -1,3 +1,9 @@
+/*
+Write a method hasOdd that takes a Set of integers as a parameter and that returns true if the set
+contains at least one odd integer, and false otherwise.
+If passed the empty set, your method should return false.
+ */
+
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
