b4a2e9979e089a86e78b1c92213cd0d971f154ac
Charles Cabral Matovu <charlescmatovu@gmail.com>
2020-10-01 22:09:02 -0400

    Added Description

--- BuildingJavaProgams/src/WriteChars.java
+++ BuildingJavaProgams/src/WriteChars.java
@@ -1,3 +1,19 @@
+/*
+  Write a method writeChars that accepts an integer parameter n
+  and that prints out n characters as follows.
+  The middle character of the output should always be an asterisk ("*").
+  If you are asked to write out an even number of characters,
+  then there will be two asterisks in the middle ("**").
+  Before the asterisk(s) you should write out less-than characters ("<").
+  After the asterisk(s) you should write out greater-than characters (">").
+  e.g.
+    writeChars(1);	*
+    writeChars(2);	**
+    writeChars(3);	<*>
+    writeChars(4);	<**>
+    writeChars(5);	<<*>>
+  */
+
 public class WriteChars {
     public static void main(String[] args) {
         writeChars(5);
