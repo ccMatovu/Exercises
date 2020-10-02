56df1411fa5926b26a539ee5f9bab2c8b38332d9
Charles Cabral Matovu <charlescmatovu@gmail.com>
2020-10-01 22:12:31 -0400

    Arrange Folders

--- BuildingJavaProgams/src/Recursion/MultiplyEvens.java
+++ BuildingJavaProgams/src/Recursion/MultiplyEvens.java
@@ -1,3 +1,15 @@
+
+/*
+  Write a method multiplyEvens that returns the product of the first n even integers.
+  You should throw an IllegalArgumentException if passed a value less than or equal to 0.
+  e.g.
+    multiplyEvens(1);	2	(2 = 2)
+    multiplyEvens(2);	8	(2 * 4 = 8)
+    multiplyEvens(3);	48	(2 * 4 * 6 = 48)
+    multiplyEvens(4);	384	(2 * 4 * 6 * 8 = 384)
+  */
+
+
 public class MultiplyEvens {
     public static void main(String[] args) {
         int product = multiplyEvens(5);
