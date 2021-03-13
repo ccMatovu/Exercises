6b6a34207bac2e3db940303ac12e4f982122c5f5
ccm-chucky <charlescmatovu@gmail.com>
2021-03-13 18:08:19 -0500

    kthlargest

--- BuildingJavaProgams/src/Arrays/kthLargest.java
+++ BuildingJavaProgams/src/Arrays/kthLargest.java
@@ -5,6 +5,30 @@ public class kthLargest {
         int k =2;
         int[] values = {74, 85, 102, 99, 101, 56, 84};
 
-        
+        System.out.println( kthLargest(k,values));
+    }
+
+    public static int kthLargest(int k, int[] values){
+
+        for(int i=0;i< values.length;i++){
+
+            for(int j=i+1;j< values.length;j++){
+
+                if(values[i] > values[j]){
+                    int temp = values[i];
+                    values[i] = values[j];
+                    values[j] = temp;
+                }
+            }
+        }
+        int kthlargest =0;
+        int z = values.length-1;
+        while(k>-1){
+            kthlargest = values[z];
+            z--;
+            k--;
+        }
+
+        return kthlargest;
     }
 }
