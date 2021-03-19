fbd6df58477e27a88bef959dd0f392e4f3e0e052
ccm-chucky <charlescmatovu@gmail.com>
2021-03-18 21:32:34 -0400

    vowelCount Exercise 7.17

--- BuildingJavaProgams/src/Arrays/vowelCount.java
+++ BuildingJavaProgams/src/Arrays/vowelCount.java
@@ -23,5 +23,32 @@ public class vowelCount {
 
     }
 
+    public static int[] vowelCount(String vowels){
+        int[] count = {0,0,0,0,0};
 
+        for(int i=0;i<vowels.length();i++){
+
+            switch (vowels.charAt(i)){
+                case 'a':
+                    count[0]++;
+                    break;
+                case 'e':
+                    count[1]++;
+                    break;
+                case 'i':
+                    count[2]++;
+                    break;
+                case 'o':
+                    count[3]++;
+                    break;
+                case 'u':
+                    count[4]++;
+                    break;
+            }
+
+        }
+
+        return count;
+
+    }
 }
