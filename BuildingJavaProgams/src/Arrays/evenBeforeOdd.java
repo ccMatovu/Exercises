b6bb52555a20e5275b8e371d07cde56098d8e14b
ccm-chucky <charlescmatovu@gmail.com>
2021-03-19 21:09:51 -0400

    evenBeforeOdd Exercise 7.18

--- BuildingJavaProgams/src/Arrays/evenBeforeOdd.java
+++ BuildingJavaProgams/src/Arrays/evenBeforeOdd.java
@@ -13,12 +13,37 @@ import java.util.Arrays;
 * solution, and do not call Arrays.sort.*/
 public class evenBeforeOdd {
     public static void main(String[] args) {
-        int[] array ={5, 4, 2, 11, 9, 10, 4, 7, 3};
+        int[] array ={5,11, 10, 8};
 
         int[] result = Arrays.copyOf(evenBeforeOdd(array),array.length);
 
         System.out.println(Arrays.toString(result));
     }
 
-    
+    public static int[] evenBeforeOdd(int[] array){
+        boolean swap = false;
+        int count =-1;
+        for(int i=0;i<array.length;i++){
+            if(swap == true){
+                swap = false;
+            }else{
+                count++;
+            }
+            for(int j=count;j<array.length;j++){
+
+                if((array[j] %2 != 0) && (j+1)<array.length){
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+
+                    swap = true;
+
+                }else{
+                    break;
+                }
+            }
+
+        }
+        return array;
+    }
 }
