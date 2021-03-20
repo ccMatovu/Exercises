994f973d378257ea2d66d658bbb1b7061eb1f8bc
ccm-chucky <charlescmatovu@gmail.com>
2021-03-20 18:07:25 -0400

    wordLengths Exercise 7.19

---
+++ BuildingJavaProgams/src/Arrays/wordLengths.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+/*
+* Write a method called wordLengths that accepts a Scanner representing an input
+* file as its argument. Your method should read from the given file, count the
+* number of letters in each token in the file, and output a result diagram of how
+* many words contain each number of letters. Use tabs before the asterisks so that
+*  they'll line up. If there are no words of a given length, omit that line from
+* the output.*/
+
+public class wordLengths {
+    public static void main(String[] args) {
+
+    }
+}
