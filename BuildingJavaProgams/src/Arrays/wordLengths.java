28aaaaa607100d17108bdd1d74d08bf0002a5487
ccm-chucky <charlescmatovu@gmail.com>
2021-03-20 18:45:01 -0400

    wordLengths Exercise 7.19

--- BuildingJavaProgams/src/Arrays/wordLengths.java
+++ BuildingJavaProgams/src/Arrays/wordLengths.java
@@ -8,8 +8,37 @@ package Arrays;
 *  they'll line up. If there are no words of a given length, omit that line from
 * the output.*/
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class wordLengths {
     public static void main(String[] args) {
 
+        int[] tokens = new int[5];
+        System.out.println(Arrays.toString(tokens));
+
+        wordLengths(new Scanner("Before sorting:\n13 23 480 -18 75\nhello how are you feeling today\n\nAfter sorting:\n-18 13 23 75 480\nare feeling hello how today you\n"));
+
+    }
+
+    public static void wordLengths(Scanner scan) {
+
+        int[] tokens = new int[80];
+
+        while(scan.hasNext()) {
+            String token = scan.next();
+            tokens[token.length() - 1]++;
+        }
+
+        for(int i = 0; i < tokens.length; i++) {
+
+            if(tokens[i] > 0) {
+                System.out.print((i+1) + ": " + tokens[i] + "\t");
+
+                for(int j = 0; j < tokens[i]; j++)
+                    System.out.print("*");
+                System.out.println();
+            }
+        }
     }
 }
