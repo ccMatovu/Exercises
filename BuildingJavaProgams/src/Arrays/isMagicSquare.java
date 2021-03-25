192c9e945ed7667c6b3449e1856cdfc79f2db913
ccm-chucky <charlescmatovu@gmail.com>
2021-03-24 21:13:49 -0400

    isMagicSquare Exercise 7.21

---
+++ BuildingJavaProgams/src/Arrays/isMagicSquare.java
@@ -0,0 +1,20 @@
+package Arrays;
+/*
+* Write a method called isMagicSquare that accepts a two-dimensional array of
+*  integers as a parameter and returns true if it is a magic square.
+* A square matrix is a magic square if it is square in shape (same number of rows
+*  as columns, and every row the same length), and all of its row, column, and
+* diagonal sums are equal. For example, [[2, 7, 6], [9, 5, 1], [4, 3, 8]] is a
+* magic square because all eight of the sums are exactly 15.*/
+
+public class isMagicSquare {
+    public static void main(String[] args) {
+        int[][] array = {{2, 7, 6},{9, 5, 1},{4, 3, 8}};
+
+        System.out.println(isMagicSquare(array));
+    }
+
+    public static boolean isMagicSquare(int[][] array){
+        
+    }
+}
