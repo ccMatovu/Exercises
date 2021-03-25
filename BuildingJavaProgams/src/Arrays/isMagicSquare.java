a07385379a0dd3c516e463a8b0b361f8a1414b73
ccm-chucky <charlescmatovu@gmail.com>
2021-03-24 21:39:27 -0400

    isMagicSquare Exercise 7.21

--- BuildingJavaProgams/src/Arrays/isMagicSquare.java
+++ BuildingJavaProgams/src/Arrays/isMagicSquare.java
@@ -9,12 +9,40 @@ package Arrays;
 
 public class isMagicSquare {
     public static void main(String[] args) {
-        int[][] array = {{2, 7, 6},{9, 5, 1},{4, 3, 8}};
+        int[][] array = {{2, 7, 6},{9, 5, 1},{4, 3,8}};
 
         System.out.println(isMagicSquare(array));
     }
 
     public static boolean isMagicSquare(int[][] array){
-        
+        if(array.length ==0){
+            return true;
+        }
+        int rowLength = array[0].length;
+        int colNum =0;
+        int previousSum=0;
+        for(int i=0;i< array.length;i++){
+            colNum++;
+            int currentSum=0;
+
+            for (int j=0;j<array[i].length;j++){
+                currentSum+=array[i][j];
+                System.out.println(currentSum);
+            }
+
+            if(i !=0 && currentSum != previousSum){
+                return false;
+            }else{
+                previousSum = currentSum;
+            }
+            if(array[i].length != rowLength){
+                return false;
+            }
+        }
+        if(colNum != rowLength){
+            return false;
+        }
+
+        return true;
     }
 }
