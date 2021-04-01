85ac8a517b7f7eff3ef294e9324041dac8f87ade
ccm-chucky <charlescmatovu@gmail.com>
2021-03-31 20:29:43 -0400

    slopePoint Exercise 8.5

--- BuildingJavaProgams/src/classes/slopePoint.java
+++ BuildingJavaProgams/src/classes/slopePoint.java
@@ -9,13 +9,29 @@ Returns the slope of the line drawn between this Point and the given other Point
 * points (x1, y1) and (x2, y2). Note that this formula fails for points with
 * identical x-coordinates, so throw an IllegalArgumentException in this case.*/
 
-public class slopePoint extends Point {
+public class slopePoint {
+    int x;
+    int y;
 
     slopePoint(int x, int y){
-        super(x,y);
+         x =x;
+         y=y;
+
     }
 
     public static void main(String[] args) {
+        slopePoint point = new slopePoint(4,4);
+        Point other = new Point(8,4);
 
+        System.out.println(point.slope(other));
+    }
+
+    private double slope(Point other) {
+        if(other.x == this.x){
+            throw new IllegalArgumentException();
+        }
+        double yDiff= other.y - this.y;
+        double xDiff = other.x -this.x;
+        return yDiff/xDiff;
     }
 }
