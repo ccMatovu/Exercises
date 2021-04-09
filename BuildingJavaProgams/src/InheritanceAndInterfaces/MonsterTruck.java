a99af86b79836d38a3bd118a9c60f9a1c5396b38
ccm-chucky <charlescmatovu@gmail.com>
2021-04-08 22:25:58 -0400

    MonsterTruck Exercise 9.4

--- BuildingJavaProgams/src/InheritanceAndInterfaces/MonsterTruck.java
+++ BuildingJavaProgams/src/InheritanceAndInterfaces/MonsterTruck.java
@@ -3,5 +3,24 @@ package InheritanceAndInterfaces;
  Don't just print/return the output; whenever possible, use inheritance to
  reuse behavior from the superclass.*/
 
-public class MonsterTruck {
+public class MonsterTruck extends Truck{
+
+    public static void main(String[] args) {
+        MonsterTruck monsterTruck = new MonsterTruck();
+        monsterTruck.m1();
+        System.out.println();
+        monsterTruck.m2();
+        System.out.println("\n"+monsterTruck.toString());
+    }
+
+    public void m1(){
+        System.out.println("monster 1");
+    }
+    public void m2(){
+        super.m1();
+        super.m2();
+    }
+    public String toString(){
+        return "monster "+super.toString();
+    }
 }
