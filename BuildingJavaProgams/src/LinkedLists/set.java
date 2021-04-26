16dd91cedb888e2906cc11eeba1d5c2001ca7cda
ccm-chucky <charlescmatovu@gmail.com>
2021-04-25 23:25:22 -0400

    set

---
+++ BuildingJavaProgams/src/LinkedLists/set.java
@@ -0,0 +1,14 @@
+package LinkedLists;
+/*
+* Write a method set that accepts an index and a value and sets the list's element at
+*  that index to have the given value. You may assume that the index is between 0
+*  (inclusive) and the size of the list (exclusive).
+
+Assume that you are adding this method to the LinkedIntList class as defined below:
+
+public class LinkedIntList {
+    private ListNode front;   // null for an empty list
+    ...
+}*/
+public class set {
+}
