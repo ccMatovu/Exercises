3a823e7030787a4f4a9c8d6500a932431726b22b
ccm-chucky <charlescmatovu@gmail.com>
2021-04-26 23:56:59 -0400

    min

---
+++ BuildingJavaProgams/src/LinkedLists/min.java
@@ -0,0 +1,13 @@
+package LinkedLists;
+/*
+Write a method min that returns the minimum value in a list of integers. If the list
+ is empty, it should throw a NoSuchElementException.
+
+Assume that you are adding this method to the LinkedIntList class as defined below:
+public class LinkedIntList {
+    private ListNode front;   // null for an empty list
+    ...
+}
+* */
+public class min {
+}
