ed05e47b10c7cbfddf650e35a40e1bb1847411bb
ccm-chucky <charlescmatovu@gmail.com>
2021-05-08 17:42:41 -0400

    IsSorted

---
+++ BuildingJavaProgams/src/LinkedLists/IsSorted.java
@@ -0,0 +1,17 @@
+package LinkedLists;
+/*
+* Write a method isSorted that returns true if the list is in sorted (nondecreasing)
+*  order and returns false otherwise. An empty list is considered to be sorted.
+
+Assume that you are adding this method to the LinkedIntList class as defined below:
+
+public class LinkedIntList {
+    private ListNode front;   // null for an empty list
+    ...
+}*/
+public class IsSorted extends LinkedIntList {
+
+    public static void main(String[] args) {
+        
+    }
+}
