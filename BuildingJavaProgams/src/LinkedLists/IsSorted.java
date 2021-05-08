b481056ba98c2ab481cd4cac63cd1696f22b25b9
ccm-chucky <charlescmatovu@gmail.com>
2021-05-08 17:57:05 -0400

    IsSorted

--- BuildingJavaProgams/src/LinkedLists/IsSorted.java
+++ BuildingJavaProgams/src/LinkedLists/IsSorted.java
@@ -12,6 +12,27 @@ public class LinkedIntList {
 public class IsSorted extends LinkedIntList {
 
     public static void main(String[] args) {
-        
+        LinkedIntList list = new IsSorted();
+        list.add(7);
+        list.add(1);
+        list.add(9);
+        list.add(5);
+        list.add(4);
+
+        System.out.println("list = "+ list.toString());
+    }
+
+    boolean isSorted(){
+        ListNode current = this.front;
+        boolean sorted = true;
+
+        while(current != null){
+            if(current.data > current.next.data){
+                return false;
+            }
+            current = current.next;
+        }
+
+        return sorted;
     }
 }
