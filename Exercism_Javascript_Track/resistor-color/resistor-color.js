6a251ac7661f9775f5c9069c1b722b8eb02d4215
ccm-chucky <charlescmatovu@gmail.com>
2021-05-31 19:43:16 -0400

    resistor color

--- Exercism_Javascript_Track/resistor-color/resistor-color.js
+++ Exercism_Javascript_Track/resistor-color/resistor-color.js
@@ -5,29 +5,10 @@
 
 export const colorCode = (color) => {
 
-    switch(color){
+  const COLORS = ['black','brown','red','orange','yellow','green','blue','violet','grey','white'];
 
-      case 'black':
-        return 0;
-      case 'brown':
-        return 1;
-      case 'red':
-        return 2;
-      case 'orange':
-        return 3;
-      case 'yellow':
-        return 4;
-      case 'green':
-        return 5;
-      case 'blue':
-        return 6;
-      case 'violet':
-        return 7;
-      case 'grey':
-        return 8;
-      case 'white':
-        return 9;
-    }
+  return COLORS.indexOf(color);
 };
 
-export const COLORS = undefined;
+export const COLORS = ['black','brown','red','orange','yellow','green','blue','violet','grey','white'];
+
