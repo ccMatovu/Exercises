5cdc2c56a6a497f5757c7254528509baebc5f93d
ccm-chucky <charlescmatovu@gmail.com>
2021-05-31 20:14:45 -0400

    resistor color duo

--- Exercism_Javascript_Track/resistor-color-duo/resistor-color-duo.js
+++ Exercism_Javascript_Track/resistor-color-duo/resistor-color-duo.js
@@ -3,6 +3,15 @@
 // convenience to get you started writing code faster.
 //
 
-export const decodedValue = () => {
-  throw new Error('Remove this statement and implement this function');
+export const decodedValue = (colors) => {
+
+  const colorArray = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
+
+  let positon1 = colorArray.indexOf(colors[0]);
+
+  let position2 = colorArray.indexOf(colors[1]);
+
+  let value = positon1.toString()+position2;
+  
+  return parseInt(value);
 };
