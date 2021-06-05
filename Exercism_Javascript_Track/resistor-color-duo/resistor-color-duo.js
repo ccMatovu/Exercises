2b10fdd602f042e0fe538f33fb4bb41cada56528
ccm-chucky <charlescmatovu@gmail.com>
2021-06-05 17:53:05 -0400

    resistor-duo

--- Exercism_Javascript_Track/resistor-color-duo/resistor-color-duo.js
+++ Exercism_Javascript_Track/resistor-color-duo/resistor-color-duo.js
@@ -7,16 +7,6 @@ import { colorCode } from '../resistor-color/resistor-color';
 
 export const decodedValue = ([color1,color2]) => {
 
-  //const colorArray = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
-
-  //let positon1 = colorArray.indexOf(colors[0]);
-
-  //let position2 = colorArray.indexOf(colors[1]);
-
-  //let value = positon1.toString()+position2;
-  
-  //return parseInt(value);
-
   let code1 = colorCode(color1);
   let code2 = colorCode(color2);
 
