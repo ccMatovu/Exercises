76518638de56f4bfcfdf4af0fab37f5e822bb547
ccm-chucky <charlescmatovu@gmail.com>
2021-06-22 21:33:31 -0400

    edit

--- Exercism_Javascript_Track/rna-transcription/rna-transcription.js
+++ Exercism_Javascript_Track/rna-transcription/rna-transcription.js
@@ -4,7 +4,23 @@
 //
 
 export const toRna = (nucleotide) => {
+    let Rna = '';
 
-  
+  for(let i =0; i<nucleotide.length; i++){
+    if(nucleotide.charAt(i) === 'G'){
+        Rna.concat('C');
+    }
+    else if(nucleotide.charAt(i)== 'C'){
+        Rna.concat('G');
+    }
+    else if(nucleotide.charAt(i)== 'T'){
+        Rna.concat('A');        
+    }
+    else if(nucleotide.charAt(i)== 'A'){
+        Rna.concat('U');        
+    }
+  }
+
+  return Rna;
 
 };
