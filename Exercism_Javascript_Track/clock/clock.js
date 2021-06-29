b909a7aee62ce4f0be84396b29ae132ed57a4194
ccm-chucky <charlescmatovu@gmail.com>
2021-06-29 01:20:10 -0400

    clock final

--- Exercism_Javascript_Track/clock/clock.js
+++ Exercism_Javascript_Track/clock/clock.js
@@ -1,16 +1,13 @@
-//
+
+  //
 // This is only a SKELETON file for the 'Clock' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
 
 export class Clock {
-  //let minutes = 0;
- // let hours = 0;
-
   constructor(hours = 0,minutes = 0) {
     this.hours = hours;
     this.minutes = minutes;
-
   }
 }
 
@@ -19,18 +16,12 @@ export class Clock {
     let hrs = this.hours;
 
     if(mins < -60){
-      console.log('befro= '+hrs);
-
       hrs = hrs - Math.floor(Math.abs((mins/60)));
-      console.log('afer= '+hrs);
-     // mins = mins + (60*Math.abs(mins));
-     mins = mins + Math.floor(Math.abs((mins/60)))*60;
-      console.log('mins beffff= '+mins);
+      mins = mins + Math.floor(Math.abs((mins/60)))*60;
     }
 
     if((mins < 0) && (mins >= -60)){
       mins = mins+60;
-      console.log('newmin= '+mins);
       hrs = hrs -1;
     } 
 
@@ -43,43 +34,25 @@ export class Clock {
     let clockHours = (hrs < 10) ? '0' + hrs : '' + hrs;
     let clockMinutes = (mins < 10) ? '0' + mins : ''+ mins;
 
-    return `${clockHours}:${clockMinutes}`;
+    return `${clockHours}:${clockMinutes}`;   
+  };
 
-    
 
-    //let hrs = (this.hours < 0) ? 24-()
-    // let totalMinutes = (Math.abs(this.hours) * 60) + (this.minutes);
-    // let totalHours = totalMinutes/60;
-    // let timeHours = (this.hours >= 0) ? Math.floor(totalHours % 24) : (Math.floor(totalHours % 24)) - 24;
-    // let timeMinutes = totalMinutes % 60;
-
-    // let realHours = (timeHours < 10) ? '0'+timeHours  : ''+timeHours;
-    // let realMinutes = (timeMinutes < 10) ? '0'+timeMinutes : ''+timeMinutes;
-
-  
-    
-  }
-
-  Clock.prototype.plus = function(addMins) {
+  Clock.prototype.plus = function(addMins){
     const newClock = new Clock(this.hours,this.minutes + addMins);
     return newClock.toString();
-  }
+  };
 
-  Clock.prototype.minus = function(subractMins) {
-    const newClock = new Clock(this.hours,this.minutes - addMins);
+
+  Clock.prototype.minus = function(subractMins){
+    const newClock = new Clock(this.hours,this.minutes - subractMins);
     return newClock.toString();
-  }
-
-  // equals() {
-  //   throw new Error('Remove this statement and implement this function');
-  // }
+  }; 
 
 
-const p =new Clock(10,0).plus(3);
-//p.plus(3);
-console.log('check');
-console.log(p.toString());
-console.log('0'+'0');
-console.log(p.toString());
+  Clock.prototype.equals = function(otherClock){
+    return this.toString() === otherClock.toString();
+  };
+
 
 
