f62292b10a99dbcae6952911e220a1584ec640a2
ccm-chucky <charlescmatovu@gmail.com>
2021-08-04 21:25:47 -0400

    test method

--- Exercism_Javascript_Track/reverse-string/reverse-string.js
+++ Exercism_Javascript_Track/reverse-string/reverse-string.js
@@ -3,12 +3,7 @@
 // convenience to get you started writing code faster.
 //
 
-export const reverseString = function(word) {
-  let characterArray = [...word];  
-  let reversed ='';
-  for(let i=characterArray.length; i > 0; i--){
-    reversed +=characterArray[i];
-  }
-  return reversed;
-};
+export const reverseString = function(word) { 
+  return word.split('').reverse().join("");
+}
 
