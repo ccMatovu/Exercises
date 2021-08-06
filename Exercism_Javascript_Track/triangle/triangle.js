ffb0d2e9892be7a780871bc0d16ad634f1caed01
ccm-chucky <charlescmatovu@gmail.com>
2021-08-05 21:33:24 -0400

    solution

--- Exercism_Javascript_Track/triangle/triangle.js
+++ Exercism_Javascript_Track/triangle/triangle.js
@@ -8,28 +8,28 @@ export class Triangle {
     this.side1 = x;
     this.side2 = y;
     this.side3 = z;
-   // this.isTriangle = true;
 
     (x + y + x) == 0 ? this.isTriangle = false : this.isTriangle = true; 
-    // let sum = x + y + z;
-    // if(sum == 0){
-    //   this.isTriangle = false;
-    // }
+
+    if(((x+y) < z) || ((x+z) <y) || ((z+y)<x)){
+      this.isTriangle = false;
+    }
+   
   }
 
   get isEquilateral() {
-    if(this.isTriangle == false){
-      return false;
-    }else if((this.side1 == this.side2) && (this.side2 == this.side3)){
+    if(this.isTriangle){
+     if((this.side1 == this.side2) && (this.side2 == this.side3)){
       return true;
-    }else{
-      return false;
     }
     }
+    return false;
+  }
 
   get isIsosceles() {
     if(this.isTriangle){
       if((this.side1 == this.side2) || (this.side1 == this.side3) || this.side3 == this.side2){
+       
         return true;
       }
     }
@@ -42,7 +42,9 @@ export class Triangle {
         return true;
       }
     }
-
     return false;
     }
+
+      
+    
 }
