98e5591e587a5ecf488619bf3adc419903752afb
ccm-chucky <charlescmatovu@gmail.com>
2021-08-06 20:52:44 -0400

    recursive solution

---
+++ Exercism_Javascript_Track/collatz-conjecture/collatz-conjecture.js
@@ -0,0 +1,30 @@
+//
+// This is only a SKELETON file for the 'Collatz Conjecture' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const steps = (number) => {
+   if(number < 1){
+    throw 'Only positive numbers are allowed';
+  }
+  else{
+    return collatz(number,0);
+  }  
+};
+
+function collatz(number,stepsCount){
+  if(number == 1){
+    return stepsCount;
+  }
+  else if( (number % 2 ) == 0){
+    console.log('cout = '+ number + stepsCount);
+    collatz(number / 2, stepsCount + 1);
+  }
+  else{
+    collatz((3 * number) +1, stepsCount + 1);
+  }
+
+  console.log("st=  "+number+stepsCount);
+  return stepsCount;
+}
+
