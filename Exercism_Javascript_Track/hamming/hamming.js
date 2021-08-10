b4cf868cfc2f824aef0004ab827067a025205a8b
ccm-chucky <charlescmatovu@gmail.com>
2021-08-09 22:31:08 -0400

    hamming.js

--- Exercism_Javascript_Track/hamming/hamming.js
+++ Exercism_Javascript_Track/hamming/hamming.js
@@ -14,20 +14,13 @@ export const compute = (dna1,dna2) => {
     throw new Error('right strand must not be empty');
   }
   else if(dna1.length != dna2.length ){
-    console.log("checked");
      throw new Error('left and right strands must be of equal length');
   }
   let dnaArray1 = dna1.split('');
- // console.log(dna1);
   let dnaArray2 = dna2.split('');
-  let hamming2 = dnaArray1.reduce((hamming,letter1,index,) => {
-   // console.log(length + " = " + index+ " letter = "+ letter1);
-    (dnaArray2[index] != letter1) ? hamming++ : hamming +=0;
-    console.log(dnaArray2[index]);
-    console.log(hamming + " = " + index+ " letter = "+ letter1);
+
+  return dnaArray1.reduce((hamming,letter,index,) => {
+    (dnaArray2[index] != letter) ? hamming++ : hamming;
     return hamming;
-
   },0);
-
-  return hamming2;
 };
