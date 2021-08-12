42d1c1eab0b8318d76dac03bba7fbe5accd7e601
ccm-chucky <charlescmatovu@gmail.com>
2021-08-11 20:15:43 -0400

    etl.js working solution

--- Exercism_Javascript_Track/etl/etl.js
+++ Exercism_Javascript_Track/etl/etl.js
@@ -3,6 +3,22 @@
 // convenience to get you started writing code faster.
 //
 
-export const transform = () => {
-  throw new Error('Remove this statement and implement this function');
+export const transform = (old) => {
+  const newScores = {};
+  
+  // console.log(old[1]);
+  // console.log(old.hasOwnProperty(5));
+  // console.log(Object.keys(old).length);
+
+  for(let i = 1; i < 11; i++){
+    //console.log(i);
+    if(old.hasOwnProperty(i)){
+      let array = old[i];
+      array.forEach((element) => {
+        //console.log('the ele is = '+element);
+        newScores[element.toLowerCase()] = i;
+      });
+    }
+  }
+  console.log(newScores);
 };
