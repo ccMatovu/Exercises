61d31c607fa3c22bf55219f48d0ca7be99dc60c0
ccm-chucky <charlescmatovu@gmail.com>
2021-08-12 20:58:48 -0400

    scrabble score js solution

--- Exercism_Javascript_Track/scrabble-score/scrabble-score.js
+++ Exercism_Javascript_Track/scrabble-score/scrabble-score.js
@@ -3,6 +3,26 @@
 // convenience to get you started writing code faster.
 //
 
-export const score = () => {
-  throw new Error('Remove this statement and implement this function');
+export const score = (letters) => {
+  const values = {
+    1: ['a','e','i','o','u','l','n','r','s','t'],
+    2: ['d','g',],
+    3: ['b','c','m','p'],
+    4: ['f','h','v','w','y'],
+    5: ['k'],
+    6: ['j','x'],
+    7: ['q','z']
+  };
+  let array = letters.split('');
+  let points = array.reduce((accu,element) =>{
+    for(let key in values){
+      let arrayValues = values[key];
+      for(let value of arrayValues){
+        if(arrayValues.includes(element)){
+          return accu + Number(key);
+        }
+      }
+    }
+  },0);
+  return points;
 };
