9a40262324d398e4fa7aa8e282677f496969be12
ccm-chucky <charlescmatovu@gmail.com>
2021-09-02 15:43:28 -0400

    word count working solution

--- Exercism_Javascript_Track/word-count/word-count.js
+++ Exercism_Javascript_Track/word-count/word-count.js
@@ -3,6 +3,39 @@
 // convenience to get you started writing code faster.
 //
 
-export const countWords = () => {
-  throw new Error('Remove this statement and implement this function');
+export const countWords = (stri) => {
+  let st = stri.replace(/\r?\n|\r|,/g,' ');
+let sta = st.split(' ');
+//console.log(sta);
+let narray = [];
+for(let i=0;i<sta.length;i++){
+	let word =sta[i];
+	let char = word.charAt(0);
+	while((char.toUpperCase() == char.toLowerCase()) &&(isNaN(char))){
+		
+		word = word.slice(1);
+		char = word.charAt(0);		
+	}
+	char = word.charAt(word.length-1);
+	while((char.toUpperCase() == char.toLowerCase()) &&(isNaN(char))){
+      word = word.slice(0,-1);
+	char = word.charAt(word.length-1);		
+	}	
+  if(word){
+	narray.push(word.toLowerCase());		
+  }
+}
+console.log(narray)
+
+
+let count = narray.reduce(function(counted,word){
+	if(word in counted){
+		 counted[word]++;
+	}else{
+		 counted[word] =1;
+	}
+	return counted
+},{});
+
+return count;
 };
