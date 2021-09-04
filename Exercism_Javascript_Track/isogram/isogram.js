a635bca3494d560b6e39bb773da309f4a218efd4
ccm-chucky <charlescmatovu@gmail.com>
2021-09-04 17:43:58 -0400

    isogram solution

--- Exercism_Javascript_Track/isogram/isogram.js
+++ Exercism_Javascript_Track/isogram/isogram.js
@@ -3,6 +3,15 @@
 // convenience to get you started writing code faster.
 //
 
-export const isIsogram = () => {
-  throw new Error('Remove this statement and implement this function');
+export const isIsogram = (word) => {
+  let chars = word.split('');
+  chars = chars.filter(char => char.toUpperCase() != char.toLowerCase());
+  for(let j = chars.length-1; j >=0; j--){
+    for(let i = j-1; i >= 0; i--){
+      if(chars[j].toLowerCase() == chars[i].toLowerCase()){
+        return false;
+      }
+    }
+  }
+  return true;
 };
