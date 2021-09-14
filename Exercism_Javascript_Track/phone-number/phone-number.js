b20605a7314f7b5a9f4519109efdaa42b80e3386
ccm-chucky <charlescmatovu@gmail.com>
2021-09-13 21:45:37 -0400

    phone number solution

--- Exercism_Javascript_Track/phone-number/phone-number.js
+++ Exercism_Javascript_Track/phone-number/phone-number.js
@@ -3,6 +3,55 @@
 // convenience to get you started writing code faster.
 //
 
-export const clean = () => {
-  throw new Error('Remove this statement and implement this function');
+export const clean = (number) => {
+  const punctuations = [ ",", ":", "!", "?"];
+  
+  let num = '';
+  for(let i = 0; i < number.length; i++){
+    if((!isNaN(number[i])) && number[i] != ' '){
+      num += number[i];
+    }
+    else if(number[i].toUpperCase() != number[i].toLowerCase()){
+      throw new Error('Letters not permitted');
+    }
+    else if((number.length > 11)&&(punctuations.includes(number[i]))){
+      throw new Error('Punctuations not permitted');
+    }
+  }
+
+
+  if(num.length < 10){
+    throw new Error('Incorrect number of digits');
+  }
+  else if(num.length == 11){
+    if(num[0] != '1'){
+      throw new Error('11 digits must start with 1');
+    }else{
+      num = num.slice(1);
+    }
+
+    if(num[0] == '0'){
+      throw new Error('Area code cannot start with zero');
+    }else if(num[0] == '1'){
+      throw new Error('Area code cannot start with one');
+    }else if(num[3] == '0'){
+      throw  new Error('Exchange code cannot start with zero');
+    }
+  }
+  else if(num.length > 11){
+    throw new Error('More than 11 digits');
+  }
+  else if(num[0] == '0'){
+    throw new Error('Area code cannot start with zero');
+  }
+  else if(num[0] == '1'){
+    throw new Error('Area code cannot start with one');
+  }
+  else if(num[3] == '0'){
+    throw new Error('Exchange code cannot start with zero');
+  }
+  else if(num[3] == '1'){
+    throw new Error('Exchange code cannot start with one');
+  }
+  return num;
 };
