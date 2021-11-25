b2313668ae00139a65d57f46f259d82e1d9ee2c4
ccm-chucky <charlescmatovu@gmail.com>
2021-11-24 21:59:12 -0500

    fix

--- Exercism_Javascript_Track/matching-brackets/matching-brackets.js
+++ Exercism_Javascript_Track/matching-brackets/matching-brackets.js
@@ -3,6 +3,39 @@
 // convenience to get you started writing code faster.
 //
 
-export const isPaired = () => {
-  throw new Error('Remove this statement and implement this function');
+export const isPaired = (combinations) => {
+  const brackets = {
+    '[':']',
+    '{':'}',
+    '(':')',
+  };
+
+  let removedBrackets = [];
+
+  for(let i = 0; i < combinations.length; i++){
+    if(Object.keys(brackets).includes(combinations[i])){
+      removedBrackets.push(combinations[i]);
+    }else if(Object.values(brackets).includes(combinations[i])){
+      removedBrackets.push(combinations[i]);
+    }
+  }
+  let arrayLen = removedBrackets.length ;
+  if((arrayLen % 2) != 0) {
+    return false;
+  }
+  console.log(removedBrackets);
+  for(let i = 0; i < arrayLen / 2; i++){
+    let bracket = removedBrackets[i];
+    let match = removedBrackets[(arrayLen-1)-i];
+    //console.log(brackets[bracket] +'match is = '+ match);
+    if(brackets[bracket] != match){
+      //console.log('yes');
+      if(brackets[bracket] == removedBrackets[i+1]){
+        return true;
+      }
+		return false;
+    }
+  }
+
+  return true;
 };
