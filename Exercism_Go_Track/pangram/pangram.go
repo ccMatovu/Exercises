315c27a9470878536b6bc1c87f15cc65ce93f62c
ccm-chucky <charlescmatovu@gmail.com>
2022-01-25 19:58:38 -0500

    pangaram

---
+++ Exercism_Go_Track/pangram/pangram.go
@@ -0,0 +1,16 @@
+package pangram
+import "strings"
+
+func IsPangram(input string) bool {
+    var alphabet = map[int32]string{
+    97: "a",98:"b",99: "c",100: "d",101: "e",102: "f",103: "g",104:"h",105:"i", 			 
+    106: "j",107: "k",108: "l",109: "m",110: "n",111: "o",112: "p",113: "q",114: "r",
+    115: "s",116: "t",117: "u",118: "v",119: "w",120: "x",121: "y",122: "z",
+}
+	input = strings.ToLower(input)
+	for _, char := range input{
+        delete(alphabet,char)
+    } 
+	return len(alphabet) == 0
+}
+
