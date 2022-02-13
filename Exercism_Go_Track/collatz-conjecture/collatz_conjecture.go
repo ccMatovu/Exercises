49a11ad333c85eb1be19e2770a131c4785cacf46
ccm-chucky <charlescmatovu@gmail.com>
2022-02-13 12:50:09 -0500

    collatz

--- Exercism_Go_Track/collatz-conjecture/collatz_conjecture.go
+++ Exercism_Go_Track/collatz-conjecture/collatz_conjecture.go
@@ -1,9 +1,14 @@
 package collatzconjecture
-
+import "errors"
 func CollatzConjecture(n int) (int, error) {
     var count int
+	if n < 1 {
+		return 0, errors.New("zero is an error")
+	}
+
 	for n != 1 {
 		count++
+		//fmt.Println(count)
 		if n%2 == 0 {
 			n = n / 2
 		} else {
