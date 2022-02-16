97e25e728635c0d39cb4df83afb99ec620fa8917
ccm-chucky <charlescmatovu@gmail.com>
2022-02-16 15:08:54 -0500

    beer song

--- Exercism_Go_Track/beer-song/beer_song.go
+++ Exercism_Go_Track/beer-song/beer_song.go
@@ -1,13 +1,43 @@
 package beer
+import (
+	"fmt"
+	"strings"
+    "errors"
+)
 
 func Song() string {
-	panic("Please implement the Song function")
-}
-
-func Verses(start, stop int) (string, error) {
-	panic("Please implement the Verses function")
+	song, _ := Verses(99, -1)
+	return song
 }
 
 func Verse(n int) (string, error) {
-	panic("Please implement the Verse function")
+    if n > 99 {
+		return "", errors.New("invalid")
+    }
+	if n == 1 {
+		return fmt.Sprintf("%d bottle of beer on the wall, %d bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n", n, n), nil
+	}
+	if n == 0 {
+		return fmt.Sprintf("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"), nil
+	}
+	if n == 2{
+        return fmt.Sprintf("%d bottles of beer on the wall, %d bottles of beer.\nTake one down and pass it around, %d bottle of beer on the wall.\n",n,n,n-1),nil
+    }
+	return fmt.Sprintf("%d bottles of beer on the wall, %d bottles of beer.\nTake one down and pass it around, %d bottles of beer on the wall.\n", n, n, n-1), nil
+}
+
+func Verses(start, stop int) (string, error) {
+	var verses strings.Builder
+    if start > 99 || start < stop || stop < -1{
+        return "",errors.New("invalid")
+    }
+
+	for  start > stop-1 && start > -1  {
+		verse, _ := Verse(start)
+		verses.WriteString(verse)
+		verses.WriteString("\n")
+		start--
+	}
+	return verses.String(), nil
+
 }
