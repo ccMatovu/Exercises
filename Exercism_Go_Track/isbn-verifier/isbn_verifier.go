256d63c96735aa2009b2bab3f7488874c29338fb
ccm-chucky <charlescmatovu@gmail.com>
2022-02-26 17:21:29 -0500

    solution 1

--- Exercism_Go_Track/isbn-verifier/isbn_verifier.go
+++ Exercism_Go_Track/isbn-verifier/isbn_verifier.go
@@ -1,5 +1,35 @@
 package isbn
 
+import "unicode"
 func IsValidISBN(isbn string) bool {
-	panic("Please implement the IsValidISBN function")
+	sop := 0
+	count := 10
+	cleanNum, clean := cleanNumber(isbn)
+	if !clean {
+		return false
+	}
+	for _, number := range cleanNum {
+		sop += number * count
+		count--
+	}
+	return sop%11 == 0
 }
+
+func cleanNumber(isbn string) ([]int, bool) {
+	cleanNum := make([]int, 0)
+	for i, num := range isbn {
+		if unicode.IsNumber(num) {
+			cleanNum = append(cleanNum, int(num-'0'))
+		} else if (i == len(isbn)-1) && (num == 'X') {
+			cleanNum = append(cleanNum, 10)
+		}
+	}
+	if len(cleanNum) != 10 {
+		return cleanNum, false
+	}
+	return cleanNum, true
+}
+
+// BenchmarkIsValidISBN-16           216424              5208 ns/op
+// PASS
+// ok      isbn    1.187s
