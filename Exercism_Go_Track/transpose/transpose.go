d4512d927b138f42b425c181df2bdde8adb45dbc
ccm-chucky <charlescmatovu@gmail.com>
2022-03-04 18:15:54 -0500

    transpose

--- Exercism_Go_Track/transpose/transpose.go
+++ Exercism_Go_Track/transpose/transpose.go
@@ -1,5 +1,25 @@
 package transpose
 
-func Transpose(input []string) []string {
-	panic("Please implement the Transpose function")
+	func Transpose(input []string) []string {
+
+	output := make([]string, 0)
+
+	for j, x := range input {
+		for i := 0; i < len(x); i++ {
+			char := string(x[i])
+			if len(output) < i+1 {
+                for space := j; space>0; space--{
+                    char = " "+char
+                } 
+				output = append(output, char)
+			} else {
+                    for len(output[i]) < j{
+                        output[i] += " "
+                    }
+				output[i] += char
+			}
+		}
+	}
+	return output
 }
+
