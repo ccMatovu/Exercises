3ac70756131bef1a444c4cfd892d8a45f46063df
ccm-chucky <charlescmatovu@gmail.com>
2022-03-07 21:32:12 -0500

    darts

--- Exercism_Go_Track/darts/darts.go
+++ Exercism_Go_Track/darts/darts.go
@@ -1,5 +1,24 @@
 package darts
 
+import "math"
+
 func Score(x, y float64) int {
-	panic("Please implement the Score function")
+    outerRadius := 10
+    middleRadius := 5
+    innerRadius := 1
+	if isInCircle(x, y, innerRadius) {
+		return 10
+	} else if isInCircle(x, y, middleRadius) {
+		return 5
+	} else if isInCircle(x, y, outerRadius) {
+		return 1
+	}
+	return 0
 }
+
+func isInCircle(x float64, y float64, radius int) bool {
+    distance := math.Sqrt(math.Pow(x, 2) + math.Pow(y, 2))
+	return distance <= float64(radius)
+}
+
+
