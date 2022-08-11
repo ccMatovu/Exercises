83c6baba3a640e3dd35125a5be0e0407e0777561
ccm-chucky <charlescmatovu@gmail.com>
2022-08-11 18:12:46 -0400

    contains duplicate

--- neetcode/src/ContainsDuplicate.java
+++ neetcode/src/ContainsDuplicate.java
@@ -18,7 +18,10 @@ import java.util.Set;
 
 public class ContainsDuplicate {
     public static void main(String[] args) {
-        System.out.println("check");
+        ContainsDuplicate check = new ContainsDuplicate();
+        System.out.println(check.containsDuplicate(new int[]{1,2,3,1}));
+        System.out.println(check.containsDuplicate(new int[]{1,2,3,4}));
+
     }
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> numsUnique = new HashSet<>();
