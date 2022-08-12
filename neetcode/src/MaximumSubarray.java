2761fee14ffac82f5c09fd1ac93cb2b178433e2a
ccm-chucky <charlescmatovu@gmail.com>
2022-08-11 20:05:13 -0400

    maximum subArray

--- neetcode/src/MaximumSubarray.java
+++ neetcode/src/MaximumSubarray.java
@@ -34,16 +34,13 @@ public class MaximumSubarray {
         System.out.println(sum(new int[]{-2,-1}));
 
     }
+
     public static int maxSubArray(int[] nums) {
         int highestSum = nums[0];
         for(int start = 0; start < nums.length; start++) {
-          //  System.out.println("start is "+ start);
             for (int end = nums.length; end > start; end--) {
                 int[] subArray = Arrays.copyOfRange(nums, start, end);
-               // System.out.println(Arrays.toString(subArray));
                 int currentSum = sum(subArray);
-               // System.out.println("current sum" + currentSum);
-              //  System.out.println("start = "+start+"  end = "+end);
                 if (currentSum > highestSum) {
                     highestSum = currentSum;
                 }
@@ -51,12 +48,28 @@ public class MaximumSubarray {
         }
         return highestSum;
     }
-    
+
     public static int sum(int[]nums){
         int s = nums[0];
         for(int i = 1; i < nums.length; i++){
             s += nums[i];
         }
         return s;
+    }
+
+    public static int maxSubArray2(int[] nums) {
+        // Initialize currMaxSum & take first element of array from which we start to do sum...
+        int maxSum = nums[0];
+        // Initialize the current sum of our subarray as nums[0]...
+        int currSum = nums[0];
+        // Traverse all the element through the loop...
+        for (int i = 1; i < nums.length; i++) {
+            // Do sum of elements contigous with curr sum...
+            // Compare it with array element to get maximum result...
+            currSum = Math.max(currSum + nums[i], nums[i]);
+            // Compare current sum and max sum.
+            maxSum = Math.max(maxSum, currSum);
+        }
+        return maxSum;      // return the contiguous subarray which has the largest sum...
     }
 }
